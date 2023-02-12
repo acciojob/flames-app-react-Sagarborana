b4a5b9f0d11e189d7f562801e8295fd6ced4bf76
Sagar Borana <93860665+Sagarborana@users.noreply.github.com>
2023-02-12 19:44:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,40 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React,{useState} from 'react'
 
-class App extends Component {
-    render() {
-        let arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"]
-        const [str1, setStr1] = useState("");
-        const [str2, setStr2] = useState("");
-        const [error, setError] = useState(false);
-        const [relation, setRelation] = useState("");
+const App = () => {
+    let arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"]
+    const [str1, setStr1] = useState("");
+    const [str2, setStr2] = useState("");
+    const [error, setError] = useState(false);
+    const [relation, setRelation] = useState("");
 
-        const evaluateRelation = () =>{
-            if(!str1 || !str2){
-                setError(true);
-            }
-            let len = str1.length + str2.length;
-            let common = 0;
-            for(let i of str1){
-                if(str2.includes(i)) common++;
-            }
-            let ans = (len - (2 * common)) % 6;
-            setRelation(arr[ans]);
+    const evaluateRelation = () =>{
+        if(!str1 || !str2){
+            setError(true);
         }
-
-        const clearInput = () =>{
-            setStr1("");
-            setStr2("");
+        let len = str1.length + str2.length;
+        let common = 0;
+        for(let i of str1){
+            if(str2.includes(i)) common++;
         }
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}\
-               <input data-testid="input1" value={str1} type="text"></input>
-               <input data-testid="input2" value={str2} type="text"></input>
-               <button data-testid="calculate_relationship" onClick={evaluateRelation}>Calculate Relationship Future</button>
-               {error ? "Please Enter valid input" : <h3 data-testid="answer">{relation}</h3>}
-               <button data-testid="clear" onClick={clearInput}>Clear</button>
-            </div>
-        )
+        let ans = (len - (2 * common)) % 6;
+        setRelation(arr[ans]);
     }
-}
 
+    const clearInput = () =>{
+        setStr1("");
+        setStr2("");
+    }
+
+    return(
+        <div id="main">
+           {/* Do not remove the main div */}\
+           <input data-testid="input1" value={str1} type="text"></input>
+           <input data-testid="input2" value={str2} type="text"></input>
+           <button data-testid="calculate_relationship" onClick={evaluateRelation}>Calculate Relationship Future</button>
+           {error ? "Please Enter valid input" : <h3 data-testid="answer">{relation}</h3>}
+           <button data-testid="clear" onClick={clearInput}>Clear</button>
+        </div>
+    )
+}
 
 export default App;
